965187454898f9a1f8ee11c5032f512771a7e9e3
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-10-19 13:36:18 -0700

    Initial Commit

---
+++ xema/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  xema
+//
+//  Created by Artem Tkachuk on 19/10/2019.
+//  Copyright © 2019 Xema. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
