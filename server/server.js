42cc0d05b10c20592153cde43baadbbf706b6ab0
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-10-19 20:26:29 -0700

    Just launched the server on App Engine. Meaning it's now available for our app to get / write data

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const { passportStrategyConfig, serializer, deserializer } = require('./config/passport');
-const flash = require('connect-flash');
-const session = require('./config/session');
+// const passport = require('passport');
+// const { passportStrategyConfig, serializer, deserializer } = require('./config/passport');
+// const flash = require('connect-flash');
+// const session = require('./config/session');
 
 const mongo = require('./database/mongo');      //db
 
@@ -19,13 +19,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 //authentication middleware
-app.use(session);
-app.use(passport.initialize());
-passport.use(passportStrategyConfig);
-app.use(passport.session());
-app.use(flash());
-passport.serializeUser(serializer);
-passport.deserializeUser(deserializer);
+// app.use(session);
+// app.use(passport.initialize());
+// passport.use(passportStrategyConfig);
+// app.use(passport.session());
+// app.use(flash());
+// passport.serializeUser(serializer);
+// passport.deserializeUser(deserializer);
 
 //Controllers
 app.use(errorController.get404);
